492020e9e8a43ba8e63bf5a367a45d4a7317405f
Venkata krishna <vkvenkat94@gmail.com>
2016-06-19 21:59:55 -0700

    Added per project search and Open link in browser.
    Signed-off-by: Venkata krishna <vkvenkat94@gmail.com>

--- DataLibrary.js
+++ DataLibrary.js
@@ -61,26 +61,25 @@ DataLibrary.prototype.getRandomEntryFromCategorySync = function(categoryFilter, 
 DataLibrary.prototype.getRandomProjectFromCategorySync = function(categoryFilter, randomCount) {
 	
 	var queryList = [], resultList = []
-		console.log(categoryFilter.toLowerCase())
-		this.jsonData['awesomeList'].forEach(function(awesomeobj) {
-			var awesomeobjCategoryTrim = awesomeobj.category.toLowerCase().trim();
-			var categoryFilterTrim = categoryFilter.toLowerCase().trim()
-			//console.log(awesomeobj.data);
-			if(awesomeobjCategoryTrim.indexOf(categoryFilterTrim) > -1) {
-			awesomeobj.data.forEach(function(awesomeobjData) {
-//				console.log(awesomeobjData.url);
-				queryList.push(awesomeobjData.url);
-			});
-		  }
-		});
-		if (randomCount < 0) {
-			randomCount = 1;
-		} else if (randomCount > queryList.length) {
-			randomCount = queryList.length;
-		}
+	console.log('CATEGORY - '+categoryFilter);
+	console.log('-----------------------------');
+	this.jsonData['awesomeList'].forEach(function(awesomeobj) {
+		var awesomeobjCategoryTrim = awesomeobj.category.toLowerCase().trim();
+		var categoryFilterTrim = categoryFilter.toLowerCase().trim();
+		if(awesomeobjCategoryTrim.indexOf(categoryFilterTrim) > -1) {
+		  awesomeobj.data.forEach(function(awesomeobjData) {
+			queryList.push(awesomeobjData.url);
+		  });
+	  }
+	});
+	if (randomCount < 0) {
+		randomCount = 1;
+	} else if (randomCount > queryList.length) {
+		randomCount = queryList.length;
+	}
 
-	resultList = array_rand.getRandomNumberInRangeSync(queryList, randomCount, 0	, queryList.length - 1);
-	return resultList;
+resultList = array_rand.getRandomNumberInRangeSync(queryList, randomCount, 0	, queryList.length - 1);
+return resultList;
 };
 
 /* This function is used to return all the entries 
@@ -88,8 +87,9 @@ DataLibrary.prototype.getRandomProjectFromCategorySync = function(categoryFilter
  * the command "aww -t <topic_name>" 
  */
 DataLibrary.prototype.getRandomEntryFromTopicSync = function(topicFilter, randomCount) {
-	var queryList = [], resultList = []
-	console.log(topicFilter.toLowerCase())
+	var queryList = [], resultList = [];
+	console.log('TOPIC - ' + topicFilter);
+	console.log('-----------------------------');
 	this.jsonData['awesomeList'].forEach(function(awesomeobj) {
 		var awesomeobjTopicTrim = awesomeobj.name.toLowerCase().trim();
 		var topicFilterTrim = topicFilter.toLowerCase().trim()
@@ -113,26 +113,44 @@ DataLibrary.prototype.getRandomEntryFromTopicSync = function(topicFilter, random
  */
 DataLibrary.prototype.getRandomProjectFromTopicSync = function(topicFilter, randomCount) {
 	
-	var queryList = [], resultList = []
-		console.log(topicFilter.toLowerCase())
-		this.jsonData['awesomeList'].forEach(function(awesomeobj) {
-			var awesomeobjTopicTrim = awesomeobj.name.toLowerCase().trim();
-			var topicFilterTrim = topicFilter.toLowerCase().trim()
-			//console.log(awesomeobj.data);
-			if(awesomeobjTopicTrim.indexOf(topicFilterTrim) > -1) {
-			awesomeobj.data.forEach(function(awesomeobjData) {
-//				console.log(awesomeobjData.url);
-				queryList.push(awesomeobjData.url);
-			});
-		  }
-		});
-		if (randomCount < 0) {
-			randomCount = 1;
-		} else if (randomCount > queryList.length) {
-			randomCount = queryList.length;
-		}
+	var queryList = [], resultList = [];
+	console.log('TOPIC - ' + topicFilter);
+	console.log('-----------------------------');
+	this.jsonData['awesomeList'].forEach(function(awesomeobj) {
+		var awesomeobjTopicTrim = awesomeobj.name.toLowerCase().trim();
+		var topicFilterTrim = topicFilter.toLowerCase().trim()
+		if(awesomeobjTopicTrim.indexOf(topicFilterTrim) > -1) {
+		  awesomeobj.data.forEach(function(awesomeobjData) {
+			  queryList.push(awesomeobjData.url);
+		  });
+	  }
+	});
+	if (randomCount < 0) {
+		randomCount = 1;
+	} else if (randomCount > queryList.length) {
+		randomCount = queryList.length;
+	}
 
 	resultList = array_rand.getRandomNumberInRangeSync(queryList, randomCount, 0	, queryList.length - 1);
 	return resultList;
 };
+
+
+
+/* This function is used to return all the entries 
+ * that matches a given Topic. It can be given by 
+ * the command "aww" 
+ */
+DataLibrary.prototype.getRandomProjectForToday = function() {
+	
+	var queryList = [], resultList = []
+		this.jsonData['awesomeList'].forEach(function(awesomeobj) {
+			awesomeobj.data.forEach(function(awesomeobjData) {
+				queryList.push(awesomeobjData.url);
+			});
+		});
+	resultList = array_rand.getRandomNumberInRangeSync(queryList, 1, 0	, queryList.length - 1);
+	return resultList;
+};
+
 module.exports = DataLibrary;
--- index.js
+++ index.js
@@ -13,32 +13,59 @@ var datalib = new DataLibrary(json_data);
 program
   .version('0.0.1')
   .option('-a, --all', 'Show all categories')
-  .option('-z, --cat [type]  ', 'Add the specified type of category','')//, 'node.js')
-  .option('-y, --top [type]  ', 'Add the specified type of topic','')//, 'node.js')
-  .option('-t, --topic [type]  ', 'Get random projects under that topic','')//, 'node.js')
+  .option('-o, --open', 'Open url in a browser')
+  .option('-z, --cat [type]  ', 'Add the specified type of category','')
+  .option('-y, --top [type]  ', 'Add the specified type of topic','')
+  .option('-t, --topic [type]  ', 'Get random projects under that topic','')
   .option('-c, --category [type]  ', 'Get random projects under that category','')
   .option('-r, --random [type]', 'Number of random entries that will be displayed', '1')
-
   .parse(process.argv);
+
  if (program.all) {
-  console.log('IN ALL');
+ 	/* Handling aww -a.
+ 	 * Fetches all categories in the list
+ 	 */
   var resultList = datalib.getCategoriesSync();
-  console.log(resultList);
-}
-else if (program.category) {
-        console.log("IN CATEGORY")
-        // console.log(program.category)
-        console.log("RANDO"+program.random);
-        var resultList = datalib.getRandomProjectFromCategorySync(program.category, program.random);
-        console.log(resultList)
+	  for(var i = 0; i < resultList.length; i++) {
+	  	console.log(resultList[i]);
+		}
+} else if (program.category) {
+  /* Handling aww -c <category_name>.
+ 	 * Fetches all projects from a given category.
+ 	 * Optional argument -r <random_count> that specifies the no. of projects to be fetched.
+ 	 * By default randomcount is '1'. So, it fetches one project from that category.
+ 	 */
+  var resultList = datalib.getRandomProjectFromCategorySync(program.category, program.random);
+  for(var i = 0; i < resultList.length; i++) {
+	  	console.log(resultList[i]);
+  }
 } else if (program.topic) {
-        console.log("IN TOPIC")
-        // console.log(program.topic)
-        console.log("RANDO"+program.random);
-        var resultList = datalib.getRandomProjectFromTopicSync(program.topic, program.random);
-        console.log(resultList)
+	/* Handling aww -t <topic_name>.
+ 	 * Fetches all projects from a given topic.
+ 	 * Optional argument -r <random_count> that specifies the no. of projects to be fetched.
+ 	 * By default randomcount is '1'. So, it fetches one project from that topic.
+ 	 */
+  var resultList = datalib.getRandomProjectFromTopicSync(program.topic, program.random);
+  for(var i = 0; i < resultList.length; i++) {
+	  	console.log(resultList[i]);
+  }
 
 } else {
-  // Default show help options
-  console.log();
+  /* Executed by default when no arguments are passed.
+   * Can also be used with aww -o in order to open the link in a browser.
+   * Fetches an awesome project that will make your day.
+   */
+
+  console.log('Hack your awesome project !');
+  console.log('-----------------------------')
+  var resultList = datalib.getRandomProjectForToday();
+  for(var i=0; i < resultList.length; i++) {
+		var resultUrl = resultList[i].toString();
+		if(program.open) {
+			console.log('Please check the link the new browser window')
+  			const opn = require('opn');
+  			opn(resultUrl);
+  		}
+  		console.log(resultUrl)
+  }
 }
